de5f63eaf9b2a680ca4b0258e4441a593e66511d
xphere <perefm@gmail.com>
2021-12-09 23:23:22 +0100

    Update to Visual Studio 2022

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -1,10 +1,8 @@
 rmdir vcpkg /s /q
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-rem update to Mar 19th: https://github.com/microsoft/vcpkg/commit/9986c28da97f316fe3c79be0538af8f46a200796
-git checkout 9986c28da97f316fe3c79be0538af8f46a200796
+rem git checkout tags/2021.05.12
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
-rem vcpkg integrate install
 vcpkg install --recurse glm entt yaml-cpp --triplet x64-windows-static
 cd..
--- 02_run_cmake.bat
+++ 02_run_cmake.bat
@@ -1,7 +1,7 @@
-rmdir EnTT_test_vs2019 /s /q
-mkdir EnTT_test_vs2019
-cd EnTT_test_vs2019
-cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 16 2019" ..
+rmdir EnTT_test_vs2022 /s /q
+mkdir EnTT_test_vs2022
+cd EnTT_test_vs2022
+cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 17 2022" ..
 cd..
 
 pause
